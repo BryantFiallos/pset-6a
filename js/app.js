e0e24019587c3a0c138632218f2a18cda121d348
unknown <bryant.fiallos@gmail.com>
2019-12-19 22:59:10 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var toDoList = [];
 
+
 window.onload = function() {
   document.getElementById('input-task').onclick = addTask;
+  document.getElementById('display') = displayList;
 }
 
 
@@ -9,6 +11,11 @@ const addTask = function () {
 
   toDoList.push(document.getElementById("user-input").value);
   document.getElementById("user-input").value = "";
+
+displayList.innerHTML = makeList;
+}
+
+const makeList = function () {
 
 var table = document.getElementById('to-do-table');
 console.log(toDoList)
