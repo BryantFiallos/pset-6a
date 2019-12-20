8344227eeaf4fed61707a01c7f57ecabfc5c077e
unknown <bryant.fiallos@gmail.com>
2019-12-19 21:11:43 -0500

    input box

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  document.getElementById('input-task').onclick = addTask;
+}
+
+
+const addTask = function () {
+  document.getElementById("myText").value = "";
+  textBox = "";
+}
