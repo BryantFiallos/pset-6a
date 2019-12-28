b114af76c2902606ee2e1c881cee3c18bc671c8f
unknown <bryant.fiallos@gmail.com>
2019-12-28 12:58:43 -0500

    finally displaying stuff

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var toDoList = [];
+var toDoListArray = [];
 
 
 
@@ -7,7 +7,7 @@ function refreshToDoList() {
 
   newInnerHtml = "";
 
-  for (var j = 0; j < toDoList; j++){
+  for (var j = 0; j < toDoListArray.length; j++){
     newInnerHtml += '<tr><td><input onclick="setItemCompleted(' + toDoListArray[j].todo_index_num + ')" type="checkbox" name="item_' + toDoListArray[j].todo_index_num + '" value="' + toDoListArray[j].todo_status + '" ' + toDoListArray[j].todo_status + '></td><td>' + toDoListArray[j].todo_index_num + ' :</td><td class="todo_item_' + toDoListArray[j].todo_status + '">' + toDoListArray[j].todo_description + '</td><td><img onclick="moveItemToTop(' + toDoListArray[j].todo_index_num + ')" src="images\\red\\24x24\\Arrow1 Up.gif"></td><td><img onclick="moveItemToBottom(' + toDoListArray[j].todo_index_num + ')" src="images\\red\\24x24\\Arrow1 Down.gif"></td><td><img onclick="deleteItem(' + toDoListArray[j].todo_index_num + ')" src="images\\red\\24x24\\trash.gif"></td></tr>';
   }
 
@@ -18,7 +18,7 @@ document.getElementById('to-do-list-display').innerHTML = newInnerHtml;
 
 
 
-function addTask() {
+function addTask(addListItem) {
 
   var task = document.getElementById("user-input").value;
 
@@ -26,9 +26,9 @@ function addTask() {
                 alert("New item cannot be empty\n");
             } else {
                 var new_item_num = toDoListArray.length
-				toDoListArray.push({todo_index_num:new_item_num, todo_status:"", todo_description:add_item.value});
+				toDoListArray.push({todo_index_num:new_item_num, todo_status:"", todo_description:task.value});
 
-        document.getElementById("user-imput").value = "";
+        document.getElementById("user-input").value = "";
 }
   refreshToDoList();
 }
